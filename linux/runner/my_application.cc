8709a51f734a69061a6ec350860b6482a309b25a
Pedro Henrique <71939458+Tdk17@users.noreply.github.com>
2026-08-19 15:56:57 -0300

    Atualiza título Linux para Cormex Exchange

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "sistem_cormex");
+    gtk_header_bar_set_title(header_bar, "Cormex Exchange");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "sistem_cormex");
+    gtk_window_set_title(window, "Cormex Exchange");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
